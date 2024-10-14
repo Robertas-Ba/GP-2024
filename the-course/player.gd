54631a8c9ff2e24f1ccc33bedd3ce5a5ef4123a4
Robertas-Ba <C24736925@mytudublin.ie>
2024-10-14 16:08:53 +0100

    topdown

--- the-course/player.gd
+++ the-course/player.gd
@@ -1,30 +1,26 @@
 extends CharacterBody2D
 
-@export var bullet_scene:PackedScene
-@export var bullet_spawn_point:Node2D
+var movespeed = 500
 
-const SPEED = 500.0
-const JUMP_VELOCITY = -400.0
-
-const TURN_RATE = 180
-
-func _physics_process(delta: float) -> void:
+func _ready():
+	pass
+func _physics_process(delta):
 	
-	var r = Input.get_axis("turn_left", "turn_right")
-	print(r)
-
-	var rot = deg_to_rad(r * TURN_RATE * delta)
-	rotate(rot)
+	var vel = Vector2.ZERO
+	if Input.is_action_just_pressed("move_forwards"):
+		vel = transform.x * movespeed
+		
+	if Input.is_action_just_pressed("move_backwards"):
+		vel = transform.x * -movespeed
+		
+	if Input.is_action_just_pressed("turn_left"):
+		vel = transform.y * movespeed
 	
-	var f = Input.get_axis("move_backwards", "move_forwards")
-
-	var vel = transform.y * f * SPEED
-	print(transform.y)
+	if Input.is_action_just_pressed("turn_right"):
+		vel = transform.y * -movespeed
+		
 	velocity = vel
 	
-	if Input.is_action_pressed("fire"):
-		var b = bullet_scene.instantiate()
-		b.global_position = bullet_spawn_point.global_position
-		get_parent().add_child(b)
-	
 	move_and_slide()
+	look_at(get_global_mouse_position())
+	
