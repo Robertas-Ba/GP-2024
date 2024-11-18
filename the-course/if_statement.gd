0ad7c0852be81126bd8fdcc16ac94667e7c8b286
C24736925 Robertas Baranauskas <C24736925@myTUDublin.ie>
2024-11-18 15:59:16 +0000

    11/18/2024

--- the-course/if_statement.gd
+++ the-course/if_statement.gd
@@ -2,9 +2,22 @@ extends Node2D
 
 func _draw() -> void:
 	var r = get_viewport_rect()
-	r.size.x = r.size.x / 2
+	var hw = r.size.x / 2
+	var hh = r.size.y / 2
+	
 	draw_rect(Rect2(r), Color.RED, true, 10, true)
-
+	var p = get_viewport().get_mouse_position()
+	print(p)
+	if p.x < hw and p.y < hh:
+		draw_rect(Rect2(hw,0,hh, r.size.y), Color.YELLOW, true)
+	elif p.x > hw and p.y > hh:
+		draw_rect(Rect2(hh,0,hw, r.size.y), Color.ORANGE, true)
+	elif p.x < hw and p.y > hh:
+		draw_rect(Rect2(hw,hh,hh, r.size.y), Color.ORANGE, true)
+	elif p.x > hw and p.y < hh:
+		draw_rect(Rect2(hh,hw,hw, r.size.y), Color.PALE_VIOLET_RED, true)
+		
+		
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,4 +25,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	queue_redraw()
 	pass
+	
+	
+			#draw_rect(Rect2(hh,0,hh, r.size.y), Color.ORANGE, true)
+		#draw_rect(Rect2(hh,hh,hh, r.size.y), Color.PALE_VIOLET_RED, true)
+		#draw_rect(Rect2(hh,0,hh, r.size.y), Color.YELLOW, true)
+		#draw_rect(Rect2(0,hh,hh, r.size.y), Color.GREEN, true)
+	#if p.y < h:
+		#draw_rect(Rect2(0,h,h, r.size.y), Color.GREEN, true)
+		#draw_rect(Rect2(h,h,h, r.size.y), Color.PALE_VIOLET_RED, true)
+	#else:
+		#draw_rect(Rect2(h,h,h, r.size.y), Color.GREEN, true)
+		#draw_rect(Rect2(0,h,h, r.size.y), Color.PALE_VIOLET_RED, true)
+	#if p.x > 400:
+		#r.size.x = r.size.x / 2
+		#draw_rect(Rect2(r), Color.RED, true, 10, true)
